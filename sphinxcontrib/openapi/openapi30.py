d57821c5bfd3bc65bcdd68db47283523b680a99b
Olivier Heurtier <olivier.heurtier@free.fr>
2025-06-20 15:52:21 +0200

    Fix pre-commit

--- sphinxcontrib/openapi/openapi30.py
+++ sphinxcontrib/openapi/openapi30.py
@@ -145,7 +145,7 @@ def _parse_schema(schema, method):
                 ret = collections.OrderedDict(example)
                 # XXX should be True to be compliant with OpenAPI
                 if (schema.get('additionalProperties', False) or 'type' not in schema) and \
-                    '...' not in example:
+                        '...' not in example:
                     # materialize in the example the fact that additional properties can be added
                     ret['...'] = '...'
                 return ret
